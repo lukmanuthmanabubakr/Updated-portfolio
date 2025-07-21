810039fb543434ca66d9c17feaf0e803b490da47
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-21 11:02:08 -1100

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -40,7 +40,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/src/Abubakr_-_CV[1].pdf"
+                href="/Abubakr_-_CV[1].pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
