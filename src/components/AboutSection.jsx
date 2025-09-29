87275e178139f438d3b961ab907fb28eefe47565
Greenwoodsy <greenwoodsy@zohomail.com>
2025-09-30 00:29:42 +0100

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -40,7 +40,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Abubakr_-_CV[1].pdf"
+                href="/public/Abubakr_Resume.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
