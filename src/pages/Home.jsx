02a5a046a4c7aec418f1b93c844b323e0af1dd5b
dev_dimeji✔ <akannioladimeji135@gmail.com>
2025-11-03 09:02:17 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle Section*/}
       <ThemeToggle />
-      {/* Background Effects */}
+      {/* Background Effects Section*/}
       <StarBackground />
 
       {/* Navbar */}
